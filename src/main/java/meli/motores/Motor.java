c53801932fea6e0a92bf6cf973a8621eca785af0
Rodrigo José Mansilla <rodrigo.mansilla@mercadolibre.com>
2019-08-12 22:14:03 -0300

    Ultimo commit de hoy

--- src/main/java/meli/motores/Motor.java
+++ src/main/java/meli/motores/Motor.java
@@ -1,16 +1,16 @@
 package meli.motores;
 
 public class Motor {
-    int potencia;
+    int potencia3;
     int temperatura;
 
     public void aumentarPotencia(int potencia) {
-        if(this.potencia + potencia > 10) {
-            this.potencia = 10;
-            this.temperatura = this.potencia * 10;
+        if(this.potencia3 + potencia > 10) {
+            this.potencia3 = 10;
+            this.temperatura = this.potencia3 * 10;
         } else {
-            this.potencia += potencia;
-            this.temperatura = this.potencia * 10;
+            this.potencia3 += potencia;
+            this.temperatura = this.potencia3 * 10;
         }
     }
 }
